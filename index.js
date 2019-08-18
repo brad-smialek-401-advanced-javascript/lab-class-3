dcbcc69736b8a2f66e08c747d5df705a525b24f2
bjwine0 <bradsmilek@gmail.com>
2019-08-18 02:32:29 -0700

    tests pass/ read me update

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
 const fileReader = require('./lib/reader.js');
 
 // Obtain and assert input
+let x = process.argv;
 let files = process.argv.slice(2);
+
+
+console.log('files', files);
 
 if( ! (files instanceof Array && files.length) ) {
   throw new Error('Invalid Args');
